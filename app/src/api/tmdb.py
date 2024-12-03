00607488d443f0f16813e9a58b56ee63ff882749
GavinHendrix <ghendrix@purdue.edu>
2024-12-03 10:19:20 -0500

    Slightly better tmdb
    Still not working but cleaned up some

--- app/src/api/tmdb.py
+++ app/src/api/tmdb.py
@@ -12,16 +12,21 @@ TMDB_API_KEY = os.getenv('TMDB_API_KEY')
 
 TMDB_BASE_URL = 'https://api.themoviedb.org/3'
 
-@tmdb_bp.route('/tmdb/movies', methods=['GET'])
+@tmdb_bp.route('/tmdb', methods=['GET'])
 def get_movies_by_criteria():
+    url = f"{TMDB_BASE_URL}/discover/movie"
+    headers = {'accept': 'application/json'}
     params = {
         'api_key': TMDB_API_KEY,
-        'langauage': 'en-US',
+        'language': 'en-US',
         'with_genres': '28' #include genres 28 - action, 35 - comedy //multiple genres can be included by using a comma
         # could add 'without_genres': , 'release_date.gte': , 'include_adult': , 'with_runtime.lte': 
     }
 
-    response = requests.get(f"{TMDB_BASE_URL}/discover/movie", params=params)
-    data = response.json()
+    response = requests.get(url=url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed request to tmdb'})
     
+    data = response.json()
     return jsonify(data)
